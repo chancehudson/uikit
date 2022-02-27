53a5d2c561fc338e5563bd7831acc64713854939
Chance Hudson <jchancehud@gmail.com>
2022-02-26 18:43:09 -0600

    refactor: remove static dir

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'static/index.ejs',
+      template: 'assets/index.ejs',
       filename: 'index.html',
       inlineSource: '.(js|css)',
     }),
