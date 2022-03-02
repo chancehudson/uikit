ff4d0a1644dc7998c12892aa1bafcdc070e76b4a
Chance Hudson <jchancehud@gmail.com>
2022-03-01 20:01:14 -0600

    fix: load ui context state on page load

--- packages/demosite/src/index.js
+++ packages/demosite/src/index.js
@@ -5,13 +5,22 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Home'
 import './index.css'
 import '@appliedzkp/kit/colors.css'
+import UIContext from '@appliedzkp/kit/interface'
 
-const RootApp = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
-)
+const RootApp = () => {
+  const ui = React.useContext(UIContext)
+  React.useEffect(() => {
+    if (typeof window !== 'undefined') {
+      ui.load()
+    }
+  }, [])
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 ReactDOM.hydrate(<RootApp />, document.getElementById('root'))
