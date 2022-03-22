178b9905a624367b774ac57f80c6611d6f8f5d4a
tkmct <1220t.takamichi@gmail.com>
2022-03-23 00:54:23 +0900

    fix: map range, color contrast

--- packages/kit/src/Stepper.js
+++ packages/kit/src/Stepper.js
@@ -6,16 +6,18 @@ export default ({ maxStep = 3, currentStep = 1, size, ...props }) => {
   const ui = useContext(UIContext)
   return (
     <div className={`stepper-container ${ui.modeCssClass} ${size ?? 'normal'}`} {...props}>
-      {[...Array(maxStep).keys()].map((v) => (
-        <>
-          <span className={`stepper-item ${v < currentStep ? 'active' : 'inactive'}`}>{v + 1}</span>
-          {v + 1 < maxStep && (
-            <div className="stepper-line">
-              <span className="stepper-line-inner"></span>
-            </div>
-          )}
-        </>
-      ))}
+      {Array(maxStep)
+        .fill()
+        .map((_, v) => (
+          <>
+            <span className={`stepper-item ${v < currentStep ? 'active' : 'inactive'}`}>{v + 1}</span>
+            {v + 1 < maxStep && (
+              <div className="stepper-line">
+                <span className="stepper-line-inner"></span>
+              </div>
+            )}
+          </>
+        ))}
     </div>
   )
 }
